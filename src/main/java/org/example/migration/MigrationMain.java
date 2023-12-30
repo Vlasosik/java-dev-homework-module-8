dc8cc1b73488aec958f203acc79521fa91bc664f
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2023-12-30 14:28:49 +0200

    added migration main methods

---
+++ src/main/java/org/example/migration/MigrationMain.java
@@ -0,0 +1,19 @@
+package org.example.migration;
+
+import org.flywaydb.core.Flyway;
+
+import static org.example.util.Constants.*;
+
+public class MigrationMain {
+    public static void main(String[] args){
+        Flyway flyway = Flyway.configure()
+                .dataSource(DB_URL, DB_USER, DB_PASS)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+        flyway.migrate();
+    }
+}
+
+
+
